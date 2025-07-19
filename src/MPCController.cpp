b244c677ed9f5d8e86567a0b821f194b4c283d14
mason1566 <22926064+mason1566@users.noreply.github.com>
2025-07-18 20:13:57 -0700

    rgb working

--- src/MPCController.cpp
+++ src/MPCController.cpp
@@ -28,12 +28,16 @@ void MPCController::Boot() {
     midi_in->open_port(observer.get_input_ports()[0]);
     midi_out.open_port(observer.get_output_ports()[0]);
 
+    for (DrumPad pad : drumpads) {
+        SetPadRGB(&pad, RGB::WHITE);
+    }
+
     while (true) {}
 };
 
 void MPCController::HandleMidiMessage(libremidi::message message) {
-    for (auto digit : message.bytes) {
-        std::cout << std::hex << std::setw(2) << (int)digit << "(" << std::dec << (int)digit << ")" << " ";
+    for (auto byte : message.bytes) {
+        std::cout << std::hex << std::setw(2) << (int)byte << "(" << std::dec << (int)byte << ")" << " ";
     }
     std::cout << std::endl;
 
@@ -47,6 +51,17 @@ void MPCController::SetPadRGB(DrumPad* pad, RGB colour) {
     //      msg_start, mfg_id, dev_id, model_id, msg_type, data_length, data_length_2, 
     //      PAD_NUM, RED_VALUE (0-127), GREEN_VALUE (0-127), BLUE_VALUE (0-127), msg_end 
     // }
-    // We are only interested in the PAD_NUM, RED_VALUE, GREEN_VALUE, and BLUE_VALUE.
+    // We are only interested in modifying the PAD_NUM, RED_VALUE, GREEN_VALUE, and BLUE_VALUE.
+    
+    // Convert colour values into unsigned char. This is because midi_out.send_message expects unsigned char values
+    unsigned char padNum = static_cast<unsigned char>(pad->padNumber);
+    unsigned char red = static_cast<unsigned char>(colour.getRed());
+    unsigned char green = static_cast<unsigned char>(colour.getGreen());
+    unsigned char blue = static_cast<unsigned char>(colour.getBlue());
 
+    unsigned char bytes[12] = { 
+        0xF0, 0x47, 0x47, 0x4A, 0x65, 0x00, 0x04, 
+        padNum, red, green, blue, 0xF7 
+    };
+    midi_out.send_message(bytes, sizeof(bytes));
 };

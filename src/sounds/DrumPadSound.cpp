11fe5a8ddf7b8c747b40ccb81b1b3fb97219f71f
mason1566 <22926064+mason1566@users.noreply.github.com>
2025-07-27 15:29:57 -0700

    add DrumPadSound helper functions

--- src/sounds/DrumPadSound.cpp
+++ src/sounds/DrumPadSound.cpp
@@ -17,3 +17,35 @@ DrumPadSound::DrumPadSound(std::vector<SoundSet>& soundSets, int soundSetIndex, 
 SoundFile& DrumPadSound::getSound() {
     return soundSets[soundSetIndex].getSoundFile(soundIndex);
 };
+
+void DrumPadSound::goNextSound() {
+    soundIndex++;
+
+    if (soundIndex >= soundSets[soundSetIndex].size()) {
+        soundIndex = 0;
+    }
+};
+
+void DrumPadSound::goPreviousSound() {
+    soundIndex--;
+
+    if (soundIndex < 0) {
+        soundIndex = soundSets[soundSetIndex].size() - 1;
+    }
+};
+
+void DrumPadSound::goNextSoundSet() {
+    soundSetIndex++;
+
+    if (soundSetIndex >= soundSets.size()) {
+        soundSetIndex = 0;
+    }
+};
+
+void DrumPadSound::goPreviousSoundSet() {
+    soundSetIndex++;
+
+    if (soundSetIndex < 0) {
+        soundSetIndex = soundSets.size() - 1;
+    }
+};
